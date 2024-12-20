1b87a58d28b21dddcddce57aa555f33d812e40a5
Chipi Media <chipi989@outlook.com>
2024-12-20 13:55:13 +0000

    feat: add username handling

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo "ENTER your username: "
+read USERNAME
+
+USER_RECORDD=$(PSQL "SELECT user_id, games played, best_game FROM users WHERE username='$USERNAME'")
+
+
+if [[ -z $USER_RECORD ]]
+then
+echo "Welcome, $USERNAME! It looks like this is your first time here."
+$PSQL "INSERT INTO users (username VALUES '$USERNAME')"
+else 
+  IFS="|" read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_RECORD"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
